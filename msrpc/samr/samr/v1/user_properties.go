23d0daeb7d8002db4b8933d07709599228a86105
oiweiwei <oiweiweiei@gmail.com>
2024-07-18 14:21:15 +0200

    msrpc: samr: add credentials parsers

---
+++ msrpc/samr/samr/v1/user_properties.go
@@ -0,0 +1,37 @@
+package samr
+
+import "context"
+
+func (o *UserProperties) AfterUnmarshalNDR(ctx context.Context) error {
+
+	if o == nil {
+		return nil
+	}
+
+	for i1 := range o.UserProperties {
+		switch v := o.UserProperties[i1].PropertyValue.GetValue().(type) {
+		case *KerberosStoredCredential:
+			for _, cred := range v.Credentials {
+				cred.KeyData = o.UserProperties[i1].PropertyValueRaw[cred.KeyOffset:][:cred.KeyLength]
+			}
+			for _, cred := range v.OldCredentials {
+				cred.KeyData = o.UserProperties[i1].PropertyValueRaw[cred.KeyOffset:][:cred.KeyLength]
+			}
+		case *KerberosStoredCredentialNew:
+			for _, cred := range v.Credentials {
+				cred.KeyData = o.UserProperties[i1].PropertyValueRaw[cred.KeyOffset:][:cred.KeyLength]
+			}
+			for _, cred := range v.ServiceCredentials {
+				cred.KeyData = o.UserProperties[i1].PropertyValueRaw[cred.KeyOffset:][:cred.KeyLength]
+			}
+			for _, cred := range v.OldCredentials {
+				cred.KeyData = o.UserProperties[i1].PropertyValueRaw[cred.KeyOffset:][:cred.KeyLength]
+			}
+			for _, cred := range v.OlderCredentials {
+				cred.KeyData = o.UserProperties[i1].PropertyValueRaw[cred.KeyOffset:][:cred.KeyLength]
+			}
+		}
+	}
+
+	return nil
+}
